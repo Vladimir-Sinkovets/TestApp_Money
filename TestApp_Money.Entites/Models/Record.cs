205df89aaca7c322d1341302aae8a227b23c958a
vladimir-sinkovets <vovasin49@gmail.com>
2023-04-12 13:51:20 +0300

    Добавьте файлы проекта.

---
+++ TestApp_Money.Entites/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp_Money.Entites.Models
+{
+    public class Record
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public Category Category { get; set; }
+        public decimal Value { get; set; }
+    }
+}
---
+++ TestApp_Money/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestApp_Money.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
