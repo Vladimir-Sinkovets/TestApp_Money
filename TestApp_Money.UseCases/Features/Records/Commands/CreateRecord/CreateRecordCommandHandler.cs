b827b2969c62a9a1dcfd06e4855c0da0035ac703
vladimir-sinkovets <vovasin49@gmail.com>
2023-04-20 10:39:24 +0300

    fix

--- TestApp_Money.UseCases/Features/Records/Commands/CreateRecord/CreateRecordCommandHandler.cs
+++ TestApp_Money.UseCases/Features/Records/Commands/CreateRecord/CreateRecordCommandHandler.cs
@@ -16,9 +16,11 @@ namespace TestApp_Money.UseCases.Features.Records.Commands.CreateRecord
             _mapper = mapper;
         }
 
-        public async Task<Guid> Handle(CreateRecordCommand request, CancellationToken cancellationToken)
+        public Task<Guid> Handle(CreateRecordCommand request, CancellationToken cancellationToken)
         {
-            var category = _context.Categories.First(x => x.Name == request.Category);
+            var category = _context.Categories.First(c => c.Name == request.Category);
+
+            var user = _context.Users.Single(u => u.Id == request.UserId);
 
             var record = new Record()
             {
@@ -27,13 +29,14 @@ namespace TestApp_Money.UseCases.Features.Records.Commands.CreateRecord
                 Id = Guid.NewGuid(),
                 Value = request.Value,
                 Category = category,
+                User = user,
             };
 
             _context.Records.Add(record);
 
-            await _context.SaveChangesAsync(CancellationToken.None);
+            _context.SaveChanges();
 
-            return record.Id;
+            return Task.FromResult(record.Id);
         }
     }
 }
